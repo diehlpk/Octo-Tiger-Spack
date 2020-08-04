4249dd8708e6626eceec3a6579084407b1fbdeb3
Patrick Diehl <me@diehlpk.de>
2020-08-04 16:46:35 -0400

    Add kokkos

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+module load cuda 
+
 cd spack
 . share/spack/setup-env.sh
 
@@ -7,4 +9,7 @@ spack install silo@4.10.2 mpi=false  %gcc@8.1.1
 
 spack install hpx@master malloc=jemalloc networking=none instrumentation=apex,papi %gcc@8.1.1 
 
-spack load hpx
+spack load hpx@master
+
+spack install kokkos@develop hpx=true cuda=true cuda_arch=70  std=14 %gcc@8.1.1
+
