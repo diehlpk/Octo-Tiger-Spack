6a323153a0fd1fbb21fe1a0e68a94595fd08f6c9
Patrick Diehl <me@diehlpk.de>
2020-08-04 14:55:53 -0400

    Add the advanced kokkos package

--- install_spack.sh
+++ install_spack.sh
@@ -4,6 +4,7 @@ git clone https://github.com/spack/spack
 cd spack
 git checkout releases/v0.15
 . share/spack/setup-env.sh
+git clone https://github.com/kokkos/kokkos-spack.git
 spack repo add kokkos-spack/kokkos
 module load gcc/8.1.0
 spack compiler find
