0ce0cc0f8c87a416144eed250a6f195b425d6cd8
Patrick Diehl <me@diehlpk.de>
2020-08-13 14:24:53 -0400

    Add local cuda package as dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
-
-module load cuda 
+module load python/3.7.0
 
 cd spack
 . share/spack/setup-env.sh
 
-spack install silo@4.10.2 mpi=false  %gcc@8.1.1
+spack install cuda@10.1.243%gcc@7.4.0
 
-spack install hpx@master malloc=jemalloc networking=none instrumentation=apex,papi %gcc@8.1.1 
+#spack install silo@4.10.2 mpi=false  %gcc@7.4.0
 
+spack install hpx@master malloc=jemalloc networking=none instrumentation=apex,papi %gcc@7.4.0
+
+spack load boost@1.73.0 
 spack load hpx@master
 
-spack install kokkos@develop hpx=true cuda=true cuda_arch=70  std=14 %gcc@8.1.1
+spack load cuda@10.1.243%gcc@7.4.0
+
+spack install kokkos@develop +hpx +cuda cuda_arch=70  std=14 +wrapper cuda@10.1.243%gcc@7.4.0
 
--- install_spack.sh
+++ install_spack.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+module load python/3.7.0
 
 git clone https://github.com/spack/spack
 cd spack
@@ -6,5 +7,8 @@ git checkout releases/v0.15
 . share/spack/setup-env.sh
 git clone https://github.com/kokkos/kokkos-spack.git
 spack repo add kokkos-spack/kokkos
-module load gcc/8.1.0
+module load gcc/7.4.0
 spack compiler find
+
+mkdir -p ~/.spack
+cp packages.yaml ~/.spack/
